30564d007f85977629be73158160dca4fb199e73
Anderley Candela <anderleyc@gmail.com>
2020-10-31 18:43:40 -0500

    Mapeo residente e informe educativo inicial

---
+++ Entities/Documento.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SISDOMI.Entities
+{
+    [BsonDiscriminator(RootClass = true)]
+    [BsonKnownTypes(
+       typeof(InformeEducativoInicial))]
+    public class Documento
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        [BsonElement("tipo")]
+        public string tipo { get; set; }
+        [BsonElement("historialcontenido")]
+        public List<string> historialcontenido { get; set; }
+        [BsonElement("creadordocumento")]
+        public string creadordocumento { get; set; }
+        [BsonElement("fechacreacion")]
+        public DateTime fechacreacion { get; set; }
+        [BsonElement("area")]
+        public string area { get; set; }
+        [BsonElement("fase")]
+        public string fase { get; set; }
+    }
+    public class ContenidoInformeEducativoInicial
+    {
+        public string situacionacademica { get; set; }
+        public string analisisacademico { get; set; }
+        public List<string> conclusiones { get; set; }
+        public List<string> anexos  { get; set; }
+        public List<Firma> firmas { get; set; }        
+    }
+
+    public class InformeEducativoInicial : Documento
+    {
+        public ContenidoInformeEducativoInicial contenido { get; set; } = new ContenidoInformeEducativoInicial();
+    }
+
+    public class Firma
+    {
+        public string urlfirma { get; set; }
+        public string nombre { get; set; }
+        public string cargo { get; set; }
+    }
+}
---
+++ Entities/Residentes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SISDOMI.Entities
+{
+    public class Residentes
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        [BsonElement("nombre")]
+        public string nombre { get; set; }
+        [BsonElement("apellido")]
+        public string apellido { get; set; }
+        [BsonElement("tipodocumento")]
+        public string tipodocumento  { get; set; }
+        [BsonElement("numerodocumento")]
+        public string numerodocumento { get; set; }
+        [BsonElement("lugarnacimiento")]
+        public string lugarnacimiento  { get; set; }
+        [BsonElement("ubigeo")]
+        public string ubigeo { get; set; }
+        [BsonElement("juzgadoprocedencia")]
+        public string juzgadoprocedencia  { get; set; }
+        [BsonElement("fechanacimiento")]
+        public DateTime fechanacimiento { get; set; }
+        [BsonElement("sexo")]
+        public string sexo { get; set; }
+        [BsonElement("telefonosreferencia")]
+        public List<Telefono>telefonosreferencia { get; set; }
+        [BsonElement("fechaingreso")]
+        public DateTime fechaingreso { get; set; }
+        [BsonElement("motivoingreso")]
+        public string motivoingreso { get; set; }
+        [BsonElement("progreso")]
+        public List<Progreso> progreso { get; set; }
+        [BsonElement("estado")]
+        public string estado { get; set; }
+
+    }
+
+    public class Telefono
+    {
+        public string numero { get; set; }
+        public string referentefamiliar { get; set; }
+    }
+}
+
