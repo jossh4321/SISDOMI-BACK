2d770daf4aebfa25fbcfc5aa3b614b5f3a532897
Stefano GL <steph.gl@hotmail.com>
2020-11-20 02:45:12 -0500

    mapeado sesion educativa

---
+++ Controllers/SesionesEducativasController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SISDOMI.DTOs;
+using SISDOMI.Entities;
+using SISDOMI.Helpers;
+using SISDOMI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http.Cors;
+
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SISDOMI.Controllers
+{
+    [Route("api/[controller]")]
+    public class SesionesEducativasController : Controller
+    {
+        private readonly SesionesEducativasService _sesioneducativa;
+
+        [HttpGet("all")]
+        public ActionResult<List<SesionEducativa>> GetAll()
+        {
+            return _sesioneducativa.GetAll();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Services/SesionesEducativasService.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using SISDOMI.DTOs;
+using SISDOMI.Entities;
+using SISDOMI.Helpers;
+using SISDOMI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace SISDOMI.Services
+{
+    public class SesionesEducativasService
+    {
+        //mapear sesiones
+        private readonly IMongoCollection<SesionEducativa> _sesioneducativa;
+
+        public List<SesionEducativa> GetAll()
+        {
+            List<SesionEducativa> sesionEducativa = new List<SesionEducativa>();
+            sesionEducativa = _sesioneducativa.Find(sesionEducativa => true).ToList();
+            return sesionEducativa;
+        }
+    }
+}
