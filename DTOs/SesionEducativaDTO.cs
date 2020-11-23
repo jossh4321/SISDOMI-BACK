e1c722ce7d3a1b76b5079a5ac8ad64e1aa8790c1
Stefano GL <steph.gl@hotmail.com>
2020-11-22 21:46:45 -0500

    SesionEducativaDTO

---
+++ DTOs/SesionEducativaDTO.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using SISDOMI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SISDOMI.DTOs
+{
+    public class SesionEducativaDTO
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public string titulo { get; set; }
+        public DateTime fechacreacion { get; set; }
+        public string area { get; set; }
+
+        public DatosResidente datosresidente { get; set; }
+    }
+
+    public class DatosResidente
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string nombrecompleto { get; set; }
+        public string tipodocumento { get; set; }
+        public string numerodocumento { get; set; }
+        public string sexo { get; set; }
+        public int faseactual { get; set; }
+        public int acogida { get; set; }
+    }
+}
