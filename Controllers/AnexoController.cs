00681539f8115787f9a321b3c699a05ac6bf7fc6
Christian Frias <christian_eduardo98@hotmail.com>
2020-11-26 19:03:39 -0500

    Anexos maquetado

---
+++ Controllers/AnexoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SISDOMI.DTOs;
+using SISDOMI.Entities;
+using SISDOMI.Models;
+using SISDOMI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SISDOMI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AnexoController : ControllerBase
+    {
+        private readonly AnexoService _anexoService;
+
+        public AnexoController(AnexoService anexoService)
+        {
+            _anexoService = anexoService;
+        }
+
+        [HttpGet("all")]
+        public async Task<ActionResult<List<AnexoDTO>>> GetAll()
+        {
+            return await _anexoService.GetAll();
+        }
+
+
+    }
+}
