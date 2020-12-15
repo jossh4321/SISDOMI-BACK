6e08ed9b19c19462d7193fadbe8780cec29f4e89
piero lavado cervantes <piero_lvdo_c@hotmail.com>
2020-12-15 09:19:16 -0500

    pequeño cambio añadiendo un campo

--- Entities/PadresFichaIngreso.cs
+++ Entities/PadresFichaIngreso.cs
@@ -10,6 +10,7 @@ namespace SISDOMI.Entities
         public string parentesco { get; set; }
         public string nombre { get; set; }
         public string apellidos { get; set; }
+        public int edad { get; set; }
         public string instruccion { get; set; }
         public string estadosalud { get; set; }
         public string relacion { get; set; }
