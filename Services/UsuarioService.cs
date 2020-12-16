9a0602f0e8718a7c6f4675673575603d002a3d15
Fabrizio Valdivia <fvaldivia4@gmail.com>
2020-12-15 19:27:11 -0500

    Se agrego funciones CambiarContraseña

--- Services/UsuarioService.cs
+++ Services/UsuarioService.cs
@@ -81,6 +81,19 @@ namespace SISDOMI.Services
             return usuario;
         }
 
+        public Usuario ModifyPassword(string id, string nuevacontrasena)
+        {
+            Usuario usuario = new Usuario();
+            var filter = Builders<Usuario>.Filter.Eq("id", id);
+            var update = Builders<Usuario>.Update
+                .Set("clave", nuevacontrasena);
+            usuario = _usuarios.FindOneAndUpdate<Usuario>(filter, update, new FindOneAndUpdateOptions<Usuario>
+            {
+                ReturnDocument = ReturnDocument.After
+            });
+            return usuario;
+        }
+
         public async Task<UsuarioDTO_UnwindRol> ObtenerUsuarioRol(string id)
         {
             var match = new BsonDocument("$match",
@@ -393,6 +406,5 @@ namespace SISDOMI.Services
             usuario = _usuarios.Find(usuario => usuario.rol == idrol).ToList();
             return usuario;
         }
-
     }
 }
